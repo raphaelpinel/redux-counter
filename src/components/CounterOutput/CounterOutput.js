877dc1dcaea3e86a91dfcac35c44be21d956b5b2
Raphael Pinel <raphaelpinel8@gmail.com>
2019-01-13 14:18:03 +0200

    capitalized Current

--- src/components/CounterOutput/CounterOutput.js
+++ src/components/CounterOutput/CounterOutput.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './CounterOuptut.module.css';
 
 const CounterOutput = props => (
-  <div className={styles.CounterOutput}>current Counter: {props.value}</div>
+  <div className={styles.CounterOutput}>Current Counter: {props.value}</div>
 );
 
 export default CounterOutput;
